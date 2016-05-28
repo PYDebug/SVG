4f7d8ee09f6c65673ef7c51e1d1ea3d95484d1b4
charlottie77 <charlottie77@163.com>
2016-05-28 16:14:01 +0800

    取色器中文提示

--- GisWeb/src/main/webapp/resources/tool.js
+++ GisWeb/src/main/webapp/resources/tool.js
@@ -1,7 +1,3 @@
-/**
- * Created by MintMint on 2016/5/8.
- */
-
 var pickup_switch = false;
 var pickup = new TCoordinatePickup(tmap,{callback:getColor});
 
@@ -11,14 +7,15 @@ $("#tool_getColor").click(function(){
         pickup.removeEvent();
        // $("#tool_getColor").css('border','  border: 2px outset buttonface;');
         $('#tool_getColor').removeClass('colorPicker');
-        $('#rgbText').html('-');
+        $('#rgbText').html('如需取色，请点击Color Picker开始取色');
         $('#svgcanvas').removeClass('svgHide');
     }
     else
     {
         pickup.addEvent();
         $("#tool_getColor").addClass('colorPicker');
-        $('#rgbText').html('Please pick color');
+        var str1 = '请点击地图上某一点取色'
+        $('#rgbText').html(str1);
         $('#svgcanvas').addClass('svgHide');
     }
     pickup_switch = !pickup_switch;
@@ -45,7 +42,7 @@ function getColor(lnglat)
             $('.Green').children('.Text').children('input').val(G);
             var B = result.B;
             $('.Blue').children('.Text').children('input').val(B);
-            $('#rgbText').html("r:"+result.R+" g:"+result.G+" b:"+result.B);
+            $('#rgbText').html("R = "+result.R+" G = "+result.G+" B = "+result.B + "依次填入上方表单中即可得到所取的颜色");
             alert("r:"+result.R+"g:"+result.G+"b:"+result.B);
 
 
@@ -54,14 +51,14 @@ function getColor(lnglat)
             "X-Auth-Token":"eyJhY2NvdW50Ijp7InVzZXJuYW1lIjoiY2xvdWRfdXNlcjEiLCJwYXNzd29yZCI6bnVsbCwiYWN0aXZlIjp0cnVlLCJyb2xlIjoiU1BFQ0lBTF9VU0VSIn0sImV4cGlyZXMiOjE0NjMyMTE1MTg5MzQsImdyYW50ZWRBdXRocyI6WyJST0xFX1NQRUNJQUxfVVNFUiJdLCJwYXNzd29yZCI6bnVsbCwiZW5hYmxlZCI6dHJ1ZSwidXNlcm5hbWUiOiJjbG91ZF91c2VyMSIsImFjY291bnROb25FeHBpcmVkIjp0cnVlLCJhY2NvdW50Tm9uTG9ja2VkIjp0cnVlLCJjcmVkZW50aWFsc05vbkV4cGlyZWQiOnRydWV9.7nUCQZ+OOpifPC1XQcUfb8oY+kAznIbBm68XmgRpN4I="
         }
     });
-    $('#rgbText').html('Calculating...Please wait.');
+    $('#rgbText').html('正在取色，请稍等。');
 }
 
 $('.Cancel').click(function() {
         pickup.removeEvent();
         // $("#tool_getColor").css('border','  border: 2px outset buttonface;');
         $('#tool_getColor').removeClass('colorPicker');
-        $('#rgbText').html('-');
+        $('#rgbText').html('如需取色，请点击Color Picker开始取色');
         $('#svgcanvas').removeClass('svgHide');
     }
 );
